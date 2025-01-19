578fea657c2b6a502b44812bf8dc506eba7d3174
shanegatz <shanegatz@gmail.com>
2025-01-19 11:48:44 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  // import HeroSection from "../lib/components/HeroSection.svelte";
+  import { HeroSection } from "$components";
+  import { ChapterPreview } from "$components";
+  import AuthorSection from "../lib/components/AuthorSection.svelte";
+    import FaqSection from "../lib/components/FaqSection.svelte";
+</script>
+
+<HeroSection />
+<ChapterPreview />
+<AuthorSection />
+<FaqSection />
